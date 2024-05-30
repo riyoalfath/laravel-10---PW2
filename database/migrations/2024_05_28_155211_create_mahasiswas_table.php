f9194f370ff97f43a343ad0cdbb7cef6ac0d6221
Ahmad Riyo Alfath <43563180+riyoalfath@users.noreply.github.com>
2024-05-30 08:42:45 +0700

    Add files via upload

---
+++ database/migrations/2024_05_28_155211_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nama');
+            $table->integer('nim')->primary();
+            $table->string('tmptlahir');
+            $table->date('tgllahir');
+            $table->double('ipk');
+            $table->char('predikat');
+            $table->string('prodi');
+            $table->string('masa studi');
+            $table->integer('suliet');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
